b7852ac0ef336205bbf82525983634de7d27a2a9
calemon <caseyalemon@gmail.com>
2019-03-06 20:16:31 -0500

    Idk what the fuck I am doing.

--- lab4/mmu.cpp
+++ lab4/mmu.cpp
@@ -7,12 +7,15 @@
 #include <mmu.h>
 
 /* Custom functions */
+static uint32_t get_pn1(uint32_t value);
+static uint32_t get_pn0(uint32_t value);
 static void set_bit(uint32_t *value, uint32_t bit_index);
 static void clear_bit(uint32_t *value, uint32_t bit_index);
 static void clear_string(char *string, uint32_t length);
 static void write_uint(uint32_t value);
 static uint32_t bit_mask(int bit_start, int bit_end);
 static void to_string(char *dest, uint32_t value);
+static void print_line_and_hex(char *pre, uint32_t value, bool newline = true);
 
 extern uint32_t MMU_TABLE[MMU_TABLE_SIZE];
 
@@ -56,11 +59,66 @@ void mmu_map(PROCESS *p){
     2) Map Program
         Same as stack
     */
-    uint32_t *space, *aligned_space, *stack_reg, *lvl1_ptr, *lvl2_ptr;
-    uint32_t root, mask, vpn1, vpn0, lvl1_pte = 0;
+    /*
+    if mapping program or stack second, check if for root + vpn[1] if the valid bit is 1, if it is follow it
+    Put in level 0 pte the program >> 2?
+    program = p->*program() & 0xfff;
+    stack_ptr = p->regs[2] & 0xfff;
+    program_end = (program + 4095 + 4096) & ~(0xfff)
+    stack_end = (stack + 4095 + 8192) & ~(0xfff)
+
+    while(){
+        level 1 stuff
+        while(){
+            level 0 stuff
+        }
+    }
+
+    Functions:
+    - get_pn1()
+    - get_pn0()
+    */
+    uint32_t program, program_end, stack, stack_end, test;
+    uint32_t *root, *program_ptr;
+    bool mapped_program = false, mapped_stack = true;
+
+    program = (uint32_t) p->program & ~(0xfff);
+    program_end = (program + (ALIGN_TO - 1) + 4096) & ~(0xfff);
+    stack = (p->regs[SP_REG]) & ~(0xfff); // NEED TO FIX
+    stack_end = (stack + (ALIGN_TO - 1) + (2 * ALIGN_TO)) & ~(0xfff);
     
     /* Find process's root space then align it */
-    root = (uint32_t) MMU_TABLE + (1024 * 10 * (p->pid-1))
+    root = MMU_TABLE + (1024 * (p->pid - 1) * 10);
+    print_line_and_hex("root before align: ", (uint32_t) root);
+
+    root = (uint32_t *) (((uint32_t) root + (ALIGN_TO - 1)) & -ALIGN_TO);
+    print_line_and_hex(" root after align: ", (uint32_t) root);
+
+    p->mmu_table = (uint32_t) root;
+
+    print_line_and_hex("program: ", program);
+
+    uint32_t vpn1 = get_pn1(program);
+    uint32_t vpn0 = get_pn0(program);
+    print_line_and_hex("p_vpn1: ", vpn1);
+    print_line_and_hex("p_vpn0: ", vpn0);
+
+    uint32_t *level0_address = root + 8192;
+    print_line_and_hex("level0_address: ", (uint32_t) level0_address);
+
+    /* Adjust 34-bit physical address to 32-bit PTE */
+    root[vpn1] = ((uint32_t) level0_address >> 2) | 1;
+    if(p->mode == ProcessMode::USER) root[vpn1] |= (1 << 4);
+    print_line_and_hex("root[vpn1]: ", root[vpn1]);
+
+    while(program < program_end){
+        level0_address[vpn0] = 5;
+        program += 4096;
+        vpn0 += 1;
+    }
+
+
+    /*
     space = MMU_TABLE + (1024 * (p->pid - 1) * 10);
     aligned_space = (uint32_t *) (((uint32_t) space + (ALIGN_TO - 1)) & -ALIGN_TO);
     p->mmu_table = (uint32_t) aligned_space;
@@ -68,7 +126,6 @@ void mmu_map(PROCESS *p){
     hex_to_string(str, (uint32_t) aligned_space);
     write_stringln(str);
 
-    /* Decompose program pointer */
     vpn1 = (((uint32_t) p->program) >> 22) & 0x3ff;
     vpn0 = (((uint32_t) p->program) >> 12) & 0x3ff;
     lvl1_ptr = aligned_space + vpn1;
@@ -91,6 +148,7 @@ void mmu_map(PROCESS *p){
     write_string("MMU_TABLE[VPN1]: ");
     hex_to_string(str, aligned_space[vpn1]);
     write_stringln(str);
+    */
 }
 
 void mmu_unmap(PROCESS *p){
@@ -121,6 +179,14 @@ void test(){
 
 
 /* Custom functions */
+static uint32_t get_pn1(uint32_t value){
+    return (value >> 22) & 0x3ff;
+}
+
+static uint32_t get_pn0(uint32_t value){
+    return (value >> 12) & 0x3ff;
+}
+
 static void set_bit(uint32_t *value, uint32_t bit_index){
     /* Left shift 1 by the given bit_index then OR the given value with the shifted bits */
     *value |= 1 << bit_index;
@@ -158,4 +224,12 @@ static uint32_t bit_mask(int bit_start, int bit_end){
     mask = (((1 << (bit_end - bit_start)) - 1) << bit_start) | (1 << bit_end);
 
     return mask;
+}
+
+static void print_line_and_hex(char *pre, uint32_t value, bool newline){
+    char hex_val[64];
+    write_string(pre);
+    hex_to_string(hex_val, value);
+    if(newline) write_stringln(hex_val);
+    else write_string(hex_val);
 }
